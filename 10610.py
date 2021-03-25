494da7989c95cd52841fe8421735577760005fbb
KimHyeji <gogo6076@naver.com>
2021-03-25 23:09:11 +0900

    update: solving

--- 10610.py
+++ 10610.py
@@ -1,10 +1,9 @@
-N = int(input())
-if N%10 != 0:
-	print(-1)
+LN = input()
+
+if '0' in LN:
+	if sum(map(int,LN)) % 3 == 0: #3의 배수판정법 모든 자리의 합이 3의 배수이다
+		print(int("".join(sorted(LN,reverse=True))))
+	else:
+		print(-1)
 else:
-	N = N//10
-	tmp = N//3*3
-	if N != tmp:
-		
-	
-	print(N*10)
+	print(-1)
