c0f714a94a3e4f65257e1c4eb63eec2a94f7afef
KimHyeji <gogo6076@naver.com>
2021-03-29 22:09:34 +0900

    Rename: folder

--- dfs_bfs/11724.py
+++ dfs_bfs/11724.py
@@ -1,16 +1,16 @@
 import sys
 sys.setrecursionlimit(10000)
-def dfs(g,c,visited):
+
+def dfs(c):
     visited[c] = True
     for i in g[c]:
         if not visited[i]:
-            dfs(g,i,visited)
-    return True
+            dfs(i)
 
-N,M = map(int,input().split())
+N,M = map(int,sys.stdin.readline().split())
 g = [[] for i in range(N+1)]
 for i in range(M):
-    u,v = map(int,input().split())
+    u,v = map(int,sys.stdin.readline().split())
     g[u].append(v)
     g[v].append(u)
 
@@ -19,6 +19,7 @@ visited = [False] * (N+1)
 cnt = 0
 for i in range(1,N+1):
     if not visited[i]:
-        dfs(g,i,visited)
+        dfs(i)
         cnt+=1
 print(cnt)
+
