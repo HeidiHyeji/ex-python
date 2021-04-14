9ad11b6209f6eb0f50022513223a690dcfbba17e
HeidiHyeji <gogo6076@naver.com>
2021-04-14 22:22:43 +0900

    Add: back tracking

--- backtracking/14889.py
+++ backtracking/14889.py
@@ -1,21 +1,21 @@
 import sys,itertools
 N = int(input())
 S = [list(map(int,sys.stdin.readline().split())) for _ in range(N)]
-MEM = [i for i in range(N)]
+MEM = set(range(0,N))
 MEMP = list(itertools.combinations(MEM,N//2))
-listmin = []
+result = float("inf")
 for i in range(len(MEMP)//2):
     starsum = 0;linksum = 0
     startmem = list(MEMP[i])
-    linkmem = [x for x in MEM if x not in startmem]
-    if tuple(linkmem) in MEMP:
-        MEMP.remove(tuple(linkmem))
-    startmem = list(itertools.combinations(startmem,2))
-    linkmem = list(itertools.combinations(linkmem,2))
-    for j in range(len(startmem)):
-        starsum += S[startmem[j][0]][startmem[j][1]]
-        starsum += S[startmem[j][1]][startmem[j][0]]
-        linksum += S[linkmem[j][0]][linkmem[j][1]]
-        linksum += S[linkmem[j][1]][linkmem[j][0]]
-    listmin.append(abs(starsum-linksum))
-print(min(listmin))
+    linkmem = list(MEM-set(startmem))
+#    if tuple(linkmem) in MEMP:
+#        MEMP.remove(tuple(linkmem))
+    for j in range(N//2):
+        for k in range(j+1,N//2):
+            starsum += S[startmem[j]][startmem[k]]
+            starsum += S[startmem[k]][startmem[j]]
+            linksum += S[linkmem[j]][linkmem[k]]
+            linksum += S[linkmem[k]][linkmem[j]]
+    tmp = abs(starsum-linksum)
+    if tmp<result: result = tmp
+print(result)
---
+++ backtracking/2580.py
@@ -0,0 +1,49 @@
+import sys
+pan = [];isusedX=[];isusedY=[];isusedN=[];box = []
+def func(cur):
+    if cur == len(box):
+        return True
+    for k in range(cur,len(box)):
+        x,y = box[k]
+        for i in range(1,10):
+            if isusedX[x][i] or isusedY[y][i] or isusedN[3*(x//3)+(y//3)][i]:
+                continue
+            isusedX[x][i] = True
+            isusedY[y][i] = True
+            isusedN[3*(x//3)+(y//3)][i] = True
+            pan[x][y] = i
+            if func(k+1): return True
+            isusedX[x][i] = False
+            isusedY[y][i] = False
+            isusedN[3*(x//3)+(y//3)][i] = False            
+    return False
+for _ in range(9):
+    pan.append(list(map(int,sys.stdin.readline().split())))
+    isusedX.append([False]*10)
+    isusedY.append([False]*10)
+    isusedN.append([False]*10)
+for i in range(9):
+    for j in range(9):
+        if pan[i][j] == 0:
+            box.append((i,j))
+        else:
+            isusedX[i][pan[i][j]] = True
+            isusedY[j][pan[i][j]] = True
+            isusedN[3*(i//3)+(j//3)][pan[i][j]] = True
+func(0)
+for i in range(9):
+    for j in range(8):
+        print(pan[i][j],end = ' ')
+    print(pan[i][8])
+'''
+반례
+0 6 0 0 0 0 2 0 9
+0 0 0 8 2 0 5 0 0
+0 1 0 9 0 3 0 0 0
+3 7 0 0 9 0 0 0 6
+1 0 0 0 0 0 0 0 8
+2 0 0 0 4 0 0 5 1
+0 0 0 5 0 4 0 9 0
+0 0 3 0 7 9 0 0 0
+5 0 9 0 0 0 0 6 0
+'''
